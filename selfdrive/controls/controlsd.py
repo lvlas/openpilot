6c8b73496af065842284ea7e166abe37bf6a1847
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2023-02-18 18:12:36 -0500

    Update controlsd.py

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -99,7 +99,7 @@ class Controls:
       get_one_can(self.can_sock)
 
       num_pandas = len(messaging.recv_one_retry(self.sm.sock['pandaStates']).pandaStates)
-      experimental_long_allowed = self.params.get_bool("ExperimentalLongitudinalEnabled") and not is_release_branch() and (self.CS_prev.cruiseState.followSettings == 4)
+      experimental_long_allowed = self.params.get_bool("ExperimentalLongitudinalEnabled") and not is_release_branch()
       self.CI, self.CP = get_car(self.can_sock, self.pm.sock['sendcan'], experimental_long_allowed, num_pandas)
     else:
       self.CI, self.CP = CI, CI.CP
@@ -140,7 +140,7 @@ class Controls:
     put_nonblocking("CarParamsPersistent", cp_bytes)
 
     # cleanup old params
-    if not self.CP.experimentalLongitudinalAvailable or is_release_branch() or self.CS_prev.cruiseState.followSettings != 4:
+    if not self.CP.experimentalLongitudinalAvailable or is_release_branch():
       self.params.remove("ExperimentalLongitudinalEnabled")
     if not self.CP.openpilotLongitudinalControl:
       self.params.remove("ExperimentalMode")
@@ -464,6 +464,12 @@ class Controls:
       self.mismatch_counter += 1
 
     self.distance_traveled += CS.vEgo * DT_CTRL
+
+    if CS.cruiseState.followSettings != 4:
+       Params().put_bool("ExperimentalLongitudinalEnabled", False)
+    else:
+       Params().put_bool("ExperimentalLongitudinalEnabled", True)
+  
 
     return CS
 

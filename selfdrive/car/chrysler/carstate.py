6ff7eb6f2d7821eeb02505e28f420bfa3fc84772
lvlas <106373597+lvlas@users.noreply.github.com>
2023-04-09 00:11:50 +0200

    Update carstate.py

--- selfdrive/car/chrysler/carstate.py
+++ selfdrive/car/chrysler/carstate.py
@@ -47,7 +47,7 @@ class CarState(CarStateBase):
     ret.vEgoRaw = cp.vl["ESP_8"]["VEHICLE_SPEED_KPH"] * CV.KPH_TO_MS
     ret.vEgo, ret.aEgo = self.update_speed_kf(ret.vEgoRaw)
     ret.standstill = bool(cp.vl["ESP_8"]["STANDSTILL"])
-    self.long_accel = cp.vl["INERTIAL_SENSOR"]["LONG_ACCEL"]
+    #self.long_accel = cp.vl["INERTIAL_SENSOR"]["LONG_ACCEL"]
     ret.gearShifter = self.parse_gear_shifter(self.shifter_values.get(cp.vl["GEAR"]["PRNDL"], None))
 
     # button presses
@@ -199,7 +199,7 @@ class CarState(CarStateBase):
       ("ACC_BRAKE_FAIL", "ESP_1"),
       ("ACC_ENG_OK", "ACCEL_RELATED_120"),
       ("ACC_ERROR", "ACC_ERROR"),
-      ("LONG_ACCEL", "INERTIAL_SENSOR"),
+      #("LONG_ACCEL", "INERTIAL_SENSOR"),
       ("TCS_ACTIVE", "ESC_ACC_COPY"),
     ]
 
@@ -225,7 +225,7 @@ class CarState(CarStateBase):
       ("ACCEL_GAS_22F", 50),
       ("ACCEL_RELATED_120", 50),
       ("ACC_ERROR", 0),
-      ("INERTIAL_SENSOR", 50),
+      #("INERTIAL_SENSOR", 50),
       ("ESC_ACC_COPY", 50),
     ]
 

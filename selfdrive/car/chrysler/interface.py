be07651d4b6ed062bdd0451d370a1e13de0ef371
lvlas <106373597+lvlas@users.noreply.github.com>
2024-07-08 09:17:25 +0200

    Hodně řezalo zatáčky, kf o hodně min

--- selfdrive/car/chrysler/interface.py
+++ selfdrive/car/chrysler/interface.py
@@ -64,7 +64,7 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.init('pid')
       ret.lateralTuning.pid.kpBP, ret.lateralTuning.pid.kiBP = [[0.0], [0.0]]
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.00025, 0.005], [0.000025, 0.0005]]
-      ret.lateralTuning.pid.kf = 0.0001
+      ret.lateralTuning.pid.kf = 0.00002
     
     # Ram
     elif candidate == CAR.RAM_1500:

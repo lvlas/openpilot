7e2c81635a9b5dcb5ba5e08850defcc952d92071
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2024-07-15 20:31:11 -0400

    Update long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -63,7 +63,7 @@ def get_jerk_factor(personality=log.LongitudinalPersonality.standard):
   elif personality==log.LongitudinalPersonality.standard:
     return 1.0
   elif personality==log.LongitudinalPersonality.aggressive:
-    return 0.5
+    return 0.75
   else:
     raise NotImplementedError("Longitudinal personality not supported")
 
@@ -74,7 +74,7 @@ def get_T_FOLLOW(personality=log.LongitudinalPersonality.standard):
   elif personality==log.LongitudinalPersonality.standard:
     return 1.45
   elif personality==log.LongitudinalPersonality.aggressive:
-    return 0.75
+    return 1.05
   else:
     raise NotImplementedError("Longitudinal personality not supported")
 

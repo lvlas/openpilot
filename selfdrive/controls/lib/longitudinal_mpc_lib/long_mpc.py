241518f9852f9ecf3189b8c9214a79c374299173
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2024-07-15 20:40:17 -0400

    Update long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -54,7 +54,7 @@ T_IDXS_LST = [index_function(idx, max_val=MAX_T, max_idx=N) for idx in range(N+1
 T_IDXS = np.array(T_IDXS_LST)
 FCW_IDXS = T_IDXS < 5.0
 T_DIFFS = np.diff(T_IDXS, prepend=[0.])
-COMFORT_BRAKE = 3.5
+COMFORT_BRAKE = 2.2
 STOP_DISTANCE = 4.0
 
 def get_jerk_factor(personality=log.LongitudinalPersonality.standard):
@@ -74,7 +74,7 @@ def get_T_FOLLOW(personality=log.LongitudinalPersonality.standard):
   elif personality==log.LongitudinalPersonality.standard:
     return 1.45
   elif personality==log.LongitudinalPersonality.aggressive:
-    return 1.05
+    return 1.0
   else:
     raise NotImplementedError("Longitudinal personality not supported")
 

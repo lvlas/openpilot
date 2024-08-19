1bf7aed436f44191a96f530db298df2a13abed74
lvlas <106373597+lvlas@users.noreply.github.com>
2024-08-19 14:47:27 +0200

    Fungovalo mazu ratia Update __init__.py

--- selfdrive/car/__init__.py
+++ selfdrive/car/__init__.py
@@ -224,9 +224,7 @@ class CarSpecs:
   centerToFrontRatio: float = 0.5
   minSteerSpeed: float = 0.0  # m/s
   minEnableSpeed: float = -1.0  # m/s
-  tireStiffnessFactor: float = 1.0
-  gearRatios: list = list[float]
-  axleRatio: float = 0.0  
+  tireStiffnessFactor: float = 1.0  
 
   def override(self, **kwargs):
     return replace(self, **kwargs)

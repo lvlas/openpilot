555674a82064d38dc5a887a1872b085ac2cb6ddb
lvlas <106373597+lvlas@users.noreply.github.com>
2024-08-22 14:34:12 +0200

    .sm Update carcontroller.py

--- selfdrive/car/chrysler/carcontroller.py
+++ selfdrive/car/chrysler/carcontroller.py
@@ -21,6 +21,7 @@ LongCtrlState = car.CarControl.Actuators.LongControlState
 
 class CarController(CarControllerBase):
   def __init__(self, dbc_name, CP, VM):
+    self.sm = messaging.SubMaster(['longitudinalPlan'])    
     self.CP = CP
     self.apply_steer_last = 0
     self.ccframe = 0
